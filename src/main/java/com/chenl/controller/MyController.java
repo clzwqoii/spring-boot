aa6f23cbb237a28cf6d382f538bfc2297511b341
clzwqoii <347928552@qq.com>
2018-01-19 18:08:18 +0800

    项目初始化

---
+++ src/main/java/com/chenl/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.chenl.controller;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMethod; 
+import org.springframework.web.bind.annotation.RestController; 
+@RestController
+public class MyController {
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String Hello(){
+        return "Hello World";
+    }
+}
+
+	 
